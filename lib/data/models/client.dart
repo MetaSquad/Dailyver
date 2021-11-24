15cf0349edcfa491fd62a750eac08221a29bc65e
benjamin <adolphe906@gmail.com>
2021-11-24 23:49:44 +0100

    update client Model

--- lib/data/models/client.dart
+++ lib/data/models/client.dart
@@ -1,47 +1,38 @@
-class Client{
-  late String _name;
-  late String _email;
-  late String _location;
-  late List<String> _cart;
-  //late List<ShoppingCart> _history;
-  late String _favoriteListId;
+class Client {
+  late String name;
+  late String email;
+  late String location;
+  late List<String> cart;
 
-  Client(String name, String email, String location){
-    _name = name;
-    _email = email;
-    _location = location;
-    _cart = [];
-    //_history =[];
-    _favoriteListId = "";
+  //TODO uncomment when ShoppingCart is done
+  //late List<ShoppingCart> history;
+  late String favoriteListId;
+
+  Client(this.name, this.email, this.location) {
+    cart = [];
+    //TODO initialize history
+    //history =[];
+    favoriteListId = "";
   }
 
-  String get favoriteListId => _favoriteListId;
-
-  set favoriteListId(String value) {
-    _favoriteListId = value;
+  Client.fromJSON(Map<String, dynamic> json) {
+    name = (json['name'] != null) ? json['name'] : "";
+    email = (json['email'] != null) ? json['email'] : "";
+    location = (json['location'] != null) ? json['location'] : "";
+    cart = ((json['cart'] as List<String>).isNotEmpty) ? json['cart'] : [];
+    //TODO gethistory from json
+    favoriteListId =
+        (json['favoriteListId'] != null) ? json['favoriteListId'] : "";
   }
 
-  List<String> get cart => _cart;
-
-  set cart(List<String> value) {
-    _cart = value;
-  }
-
-  String get location => _location;
-
-  set location(String value) {
-    _location = value;
-  }
-
-  String get email => _email;
-
-  set email(String value) {
-    _email = value;
-  }
-
-  String get name => _name;
-
-  set name(String value) {
-    _name = value;
+  Map<String, dynamic> toJSON() {
+    return {
+      'name': name,
+      'email': email,
+      'location': location,
+      'cart': cart,
+      //TODO convert history to JSON
+      'favoriteListId': favoriteListId,
+    };
   }
 }
