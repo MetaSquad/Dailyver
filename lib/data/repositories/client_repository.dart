8b240479ec2a1fcf5d5f0b05891f39ff8721e542
benjamin <adolphe906@gmail.com>
2021-11-25 10:24:45 +0100

    Add queries for create, connect and disconnect client

--- lib/data/repositories/client_repository.dart
+++ lib/data/repositories/client_repository.dart
@@ -2,19 +2,36 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dailyver/utils/constants.dart';
-class ClientRepository{
-  final _clientDB = FirebaseFirestore.instance.collection(Constants.client_reference);
+import 'package:firebase_auth/firebase_auth.dart';
 
-  Future<QuerySnapshot> getAllClient(){
+class ClientRepository {
+  final _authinstance = FirebaseAuth.instance;
+  final _clientDB =
+      FirebaseFirestore.instance.collection(Constants.client_reference);
+
+  Future<QuerySnapshot> getAllClient() {
     return _clientDB.get();
   }
 
-  Future<QuerySnapshot> getClientFromEmail(String email){
-    return _clientDB.where('email',isEqualTo: email).get();
+  Future<QuerySnapshot> getClientFromEmail(String email) {
+    return _clientDB.where('email', isEqualTo: email).get();
   }
 
-  Future<DocumentReference> addClient(Map<String,dynamic> client){
+  Future<DocumentReference> addClient(Map<String, dynamic> client) {
     return _clientDB.add(client);
   }
 
+  Future<UserCredential> connectClient(String email, String password) {
+    return _authinstance.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<UserCredential> registerClient(String email, String password) {
+    return _authinstance.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> signOutClient() {
+    return _authinstance.signOut();
+  }
 }
--- lib/data/services/client_service.dart
+++ lib/data/services/client_service.dart
@@ -5,19 +5,39 @@ class ClientService {
   final ClientRepository _repository = ClientRepository();
 
   Future<List<Client>> getAllClient() async {
-    return await _repository
-        .getAllClient()
-        .then((value) => value.docs.map((e) => Client.fromJSON(e.data() as Map<String,dynamic>)).toList());
+    return await _repository.getAllClient().then((value) => value.docs
+        .map((e) => Client.fromJSON(e.data() as Map<String, dynamic>))
+        .toList());
   }
 
   Future<Client> getClientFromEmail(String email) async {
     return await _repository.getClientFromEmail(email).then((value) => value
         .docs
-        .map((e) => Client.fromJSON(e.data() as Map<String,dynamic>))
+        .map((e) => Client.fromJSON(e.data() as Map<String, dynamic>))
         .toList()[0]);
   }
 
   Future<void> addClient(Client client) async {
     await _repository.addClient(client.toJSON());
+  }
+
+  Future<Client> connectClient(String email, String password) async {
+    String clientEmail = "";
+    await _repository
+        .connectClient(email, password)
+        .then((value) => clientEmail = value.user!.email!);
+    return await getClientFromEmail(clientEmail);
+  }
+
+  Future<Client> registerClient(String email, String password) async {
+    String clientEmail = "";
+    await _repository
+        .registerClient(email, password)
+        .then((value) => clientEmail = value.user!.email!);
+    return await getClientFromEmail(clientEmail);
+  }
+
+  Future<void> signOut() async {
+    await _repository.signOutClient();
   }
 }
